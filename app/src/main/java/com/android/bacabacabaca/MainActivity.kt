5760016851f82b9bdefbdc5f10a0a97b7cede6e3
nanang <nanangarif404@gmail.com>
2023-03-20 10:31:33 +0700

    Initial commit

---
+++ app/src/main/java/com/android/bacabacabaca/MainActivity.kt
@@ -0,0 +1,26 @@
+package com.android.bacabacabaca
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.android.bacabacabaca.databinding.ActivityMainBinding
+import com.android.bacabacabaca.feat_hurufkapital.AbjadKapitalActivity
+
+class MainActivity : AppCompatActivity() {
+
+    private val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(binding.root)
+
+        binding.apply {
+            btnAbjadKapital.setOnClickListener {
+                startActivity(Intent(this@MainActivity, AbjadKapitalActivity::class.java))
+            }
+        }
+
+    }
+
+
+}
---
+++ app/src/main/java/com/android/bacabacabaca/model/Abjad.kt
@@ -0,0 +1,8 @@
+package com.android.bacabacabaca.model
+
+data class Abjad(
+    var id: Int = 0,
+    var abjad: String = "",
+    var background: String = "",
+    var suara: String = ""
+)
